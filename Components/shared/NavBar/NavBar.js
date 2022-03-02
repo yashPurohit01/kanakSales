456ffcda1075f57f685f3120d6498a9e20389e83
Yash Mukund Purohit <j.samkumar@shivila.com>
2022-03-02 21:39:13 +0530

    nav

--- Components/shared/NavBar/NavBar.js
+++ Components/shared/NavBar/NavBar.js
@@ -6,23 +6,24 @@ import styles from './navbar.module.scss'
 function NavBar() {
     return (
         <Navbar bg="red" expand={false} >
-            <Container fluid className={styles.navContainer}>
-                <Navbar.Toggle aria-controls="offcanvasNavbar" className={styles.navIcons}  />
+            <Container fluid className={styles.navContainer}  >
+                <Navbar.Toggle aria-controls="offcanvasNavbar"  className={styles.navIcons}  />
                 <Navbar.Offcanvas
                     id="offcanvasNavbar"
                     aria-labelledby="offcanvasNavbarLabel"
                     placement="end"
                     className={styles.navbarSideBar}
+                    style={{width:200}}
                 >
                     <Offcanvas.Header closeButton className={styles.navbarHead}>
                         <Offcanvas.Title id="offcanvasNavbarLabel">Menu</Offcanvas.Title>
                     </Offcanvas.Header>
-                    <Offcanvas.Body  className={styles.navbarBody}>
+                    <Offcanvas.Body   className={styles.navbarBody} >
                         <Nav className="justify-content-end flex-grow-1 pe-3">
                             <Link href="/"><a>Home</a></Link>
                             <Link href="/products"><a>Products</a></Link>
-                            <Link href="/about"><a>About us</a></Link>
-                            <Link href="/contact"><a>Contact us</a></Link>
+                            <Link href="/about"><a>About </a></Link>
+                            <Link href="/contact"><a> Contact</a></Link>
                             
                         </Nav>
                       
